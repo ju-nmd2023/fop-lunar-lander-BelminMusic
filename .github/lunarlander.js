1f97e8097f87346c15dfa2a2a4e7de40d6575984
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2024-02-20 14:02:22 +0100

    rocket

---
+++ .github/lunarlander.js
@@ -0,0 +1,27 @@
+background("#ade8f4");
+
+// Wings
+fill("#fb6107");
+triangle(185, 200, 175, 240, 195, 240);
+triangle(215, 200, 205, 240, 225, 240); 
+
+//Body
+fill("#d90429");
+ellipse(200, 200, 50, 100);
+
+//Window
+fill("#f8f9fa");
+ellipse(200, 180, 20, 20);
+
+// Head inside the window
+fill("#000000"); 
+ellipse(200, 185, 8, 8);
+
+//eyes
+fill("#ffffff");
+ellipse(199, 185, 2, 2); // Left eye
+ellipse(201, 185, 2, 2); // Right eye
+
+//Middle detail
+fill("#343a40");
+rect(175, 200, 50, 10);
